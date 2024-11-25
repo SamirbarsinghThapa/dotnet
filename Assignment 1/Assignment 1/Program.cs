395ae5a94cba86f767351a3d4c250d84c2cb9bd6
SamirbarsinghThapa <samirbarthapa@gmail.com>
2024-11-25 08:38:13 +0545

    assignment 1

---
+++ Assignment 1/Assignment 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Firstnumber = 10, Secondnumber = 15;
+
+            Console.WriteLine($"Before Swap: Firstnumber = {Firstnumber}, Secondnumber = {Secondnumber}");
+
+            Firstnumber = Firstnumber + Secondnumber; 
+            Secondnumber = Firstnumber - Secondnumber; 
+            Firstnumber = Firstnumber - Secondnumber; 
+
+            Console.WriteLine($"After Swap: Firstnumber = {Firstnumber}, Secondnumber = {Secondnumber}");
+        }
+    }
+}
